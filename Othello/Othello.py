f3ff1eaf18077b47a919f1fcf7e82804d7c739c4
Benoît Coste <benoit.coste@epfl.ch>
2020-12-06 22:55:44 +0100

    doing stuff

--- Othello/Othello.py
+++ Othello/Othello.py
@@ -1,4 +1,4 @@
-iimport sys
+import sys
 import math
 import numpy as np
 import random
@@ -30,14 +30,14 @@ def is_terminal_node(grid, config):
 def minimax(node, depth, maximizingPlayer, mark, config):
     is_terminal = is_terminal_node(node, config)
     if depth == 0 or is_terminal:
-        value = get_heuristic(node, mark, config) 
+        value = get_heuristic(node, mark, config)
     elif maximizingPlayer:
         v_moves = valid_moves(node, mark, config)
         value = -np.Inf
         for move in v_moves:
             child, child_taken = play_piece(node, move, mark, config)
             value = max(value, minimax(child, depth-1, False, mark, config))
-            print(v_moves, file=sys.stderr, flush=True) 
+            print(v_moves, file=sys.stderr, flush=True)
     else:
         op_v_moves = valid_moves(node, 1-mark, config)
         value = np.Inf
@@ -69,26 +69,26 @@ def count_corners(grid, mark, config):
 # Helper function for listing vald moves
 def valid_moves(grid, mark, config):
     return [move
-            for move in itertools.product(range(config.columns), range(confg.columns)) 
+            for move in itertools.product(range(config.columns), range(confg.columns))
             if grid[move] == '.' and play_piece(grid, move, mark, config)[1] > 0]
-   
-    
+
+
 # Helper for knowing how much are taken per window
 def taken_in_move(window, mark, config):
     count = 0
     nb_taken = 0
     for win in window[1:]:
         if win == '.' : break
-        elif win != str(mark) and win != '.' : 
+        elif win != str(mark) and win != '.' :
             count += 1
-        elif win == str(mark) : 
+        elif win == str(mark) :
             nb_taken = count
-            break     
-    return nb_taken 
+            break
+    return nb_taken
 
 # Helper for knowing result of a move
 def play_piece(grid, move, mark, config) :
-    x = move[1] 
+    x = move[1]
     y = move[0]
     next_grid = grid.copy()
     taken_this_move = 0
@@ -99,7 +99,7 @@ def play_piece(grid, move, mark, config) :
         if taken > 0:
             next_grid[y, x:x+taken] = mark
             taken_this_move += taken
-    # horizontal -  
+    # horizontal -
     window = list(grid[y, x::-1])
     if len(window) > 2 :
         if window[1] == str(1 - mark) :
@@ -108,8 +108,8 @@ def play_piece(grid, move, mark, config) :
                 next_grid[y, x:x-taken:-1] = mark
                 taken_this_move += taken
     # vertical +
-    window = list(grid[y::, x]) 
-    if type(window) == list and len(window) > 2 :
+    window = list(grid[y::, x])
+    if len(window) > 2 :
         if window[1] == str(1 - mark) :
             taken = taken_in_move(window, mark, config)
             if taken > 0:
@@ -117,48 +117,28 @@ def play_piece(grid, move, mark, config) :
                 taken_this_move += taken
     # vertical -
     window = list(grid[y::-1,x ])
-    if type(window) == list and len(window) > 2 :
+    if len(window) > 2 :
         if window[1] == str(1 - mark):
             taken = taken_in_move(window, mark, config)
-            if taken > 0: 
+            if taken > 0:
                 next_grid[y:y-taken+1:-1,x] = mark
                 taken_this_move += taken
     # diagonal up right
-    window = list(np.flipud(grid[:y+1, x:]).diagonal())
-    if type(window) == list and len(window) > 2 :
-        if window[1] == str(1 - mark) :
+
+    for (a, b), min_ for zip(itertools.product([1, -1], [1, -1]),
+                             [[config.columns - y - 1, config.columns - x - 1]
+                              [config.columns - y - 1, x]
+                              [y, config.columns - x - 1]
+                              [y, x]]):
+        diag = [[y + a * i for i in range(min_)],
+                [x + b * i for i in range(min_)]]
+        window = grid[diag]
+        if len(window) > 2 and window[1] == str(1 - mark) :
             taken = taken_in_move(window, mark, config)
             if taken > 0:
-                for row in range(x,x+taken+1) :
-                    next_grid[y-row+x,row ] = mark
+                next_grid[diag[taken]] = mark
                 taken_this_move += taken
-    # diagonal down right
-    window = list(grid[y:, x:].diagonal())
-    if type(window) == list and len(window) > 2 :
-        if window[1] == str(1 - mark) :
-            taken = taken_in_move(window, mark, config)
-            if taken > 0: 
-                for row in range(x,x+taken+1) :
-                    next_grid[y+row-x, row ] = mark
-                taken_this_move += taken
-    # diagonal up left
-    window = list(grid[y::-1,x::-1].diagonal())
-    if type(window) == list and len(window) > 2 :
-        if window[1] == str(1 - mark) :
-            taken = taken_in_move(window, mark, config)
-            if taken > 0: 
-                for row in range(x,x-taken-1, -1) :
-                    next_grid[y+row-x, row ] = mark
-                taken_this_move += taken
-    # diagonal down left
-    window = list(np.fliplr(grid[y::, :x+1]).diagonal())
-    if type(window) == list and len(window) > 2 :
-        if window[1] == str(1 - mark) :
-            taken = taken_in_move(window, mark, config)
-            if taken > 0 :
-                for row in range(x,x-taken-1, -1) :
-                    next_grid[y-row+x, row ] = mark
-                taken_this_move += taken   
+
     return next_grid, taken_this_move
 
 # Helper function for score_move: calculates value of heuristic for grid
@@ -174,66 +154,62 @@ def get_heuristic(grid, mark, config):
 
 def convert_move(move) :
     y = move[0] + 1
-    x = move[1] 
-    order = ""
-    if x == 0 : order = "a"+str(y)
-    if x == 1 : order = "b"+str(y)
-    if x == 2 : order = "c"+str(y)
-    if x == 3 : order = "d"+str(y)
-    if x == 4 : order = "e"+str(y)
-    if x == 5 : order = "f"+str(y)
-    if x == 6 : order = "g"+str(y)
-    if x == 7 : order = "h"+str(y)
-    return order
+    x = move[1]
+    return "abcdefgh"[x] + str(y)
 
 N_STEPS = 2
 def agent(obs, config):
     # Convert the board to a 2D grid ()
     # Get list of valid moves
     v_moves = valid_moves(grid, obs.mark, config)
-    print("v_moves: ", file=sys.stderr, flush=True) 
-    print(v_moves, file=sys.stderr, flush=True) 
+    print("v_moves: ", file=sys.stderr, flush=True)
+    print(v_moves, file=sys.stderr, flush=True)
 
     # Use the heuristic to assign a score to each possible board in the next turn
     scores = dict(zip(v_moves, [score_move(grid, move, obs.mark, config, N_STEPS) for move in v_moves]))
     # Get a list of columns (moves) that maximize the heuristic
-    print("scores: ", file=sys.stderr, flush=True) 
-    print(scores, file=sys.stderr, flush=True) 
-        
+    print("scores: ", file=sys.stderr, flush=True)
+    print(scores, file=sys.stderr, flush=True)
+
     max_moves = [key for key in scores.keys() if scores[key] == max(scores.values())]
     # Select at random from the maximizing columns
-    print("max_moves :", file=sys.stderr, flush=True) 
+    print("max_moves :", file=sys.stderr, flush=True)
 
-    print(max_moves, file=sys.stderr, flush=True) 
+    print(max_moves, file=sys.stderr, flush=True)
 
     return random.choice(max_moves)
 
-# game loop
-while True:
-    obs.board = []
-    for i in range(board_size):
-        line = input()  # rows from top to bottom (viewer perspective).
-        obs.board.extend(line)            
-    action_count = int(input())  # number of legal actions for this turn.
-    grid = np.asarray(obs.board).reshape(config.columns, config.columns)
+def main():
+    # game loop
+    while True:
+        obs.board = []
+        for i in range(board_size):
+            line = input()  # rows from top to bottom (viewer perspective).
+            obs.board.extend(line)
+        action_count = int(input())  # number of legal actions for this turn.
+        grid = np.asarray(obs.board).reshape(config.columns, config.columns)
 
 
-    for i in range(action_count):
-        action = input()  # the action
-    print(np.sum(grid == '.'), file=sys.stderr, flush=True) 
+        for i in range(action_count):
+            action = input()  # the action
+        print(np.sum(grid == '.'), file=sys.stderr, flush=True)
 
-    temps1 = datetime.now()
-    move_to_play = agent(obs, config)
-    order = convert_move(move_to_play)
-    print(move_to_play, file=sys.stderr, flush=True)
-    temps2 = datetime.now()
-    delay = temps2 - temps1
-    print(delay, file=sys.stderr, flush=True)
-
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True) 
+        temps1 = datetime.now()
+        move_to_play = agent(obs, config)
+        order = convert_move(move_to_play)
+        print(move_to_play, file=sys.stderr, flush=True)
+        temps2 = datetime.now()
+        delay = temps2 - temps1
+        print(delay, file=sys.stderr, flush=True)
 
 
-    # a-h1-8
-    print(order)
+        # Write an action using print
+        # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+        # a-h1-8
+        print(order)
+
+
+if __name__ == '__main__':
+    main()
