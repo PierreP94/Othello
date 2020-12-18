eee15949f477893a48b9b07a32024f4578c032f6
PierreP94 <poitevin.p@gmail.com>
2020-12-18 20:46:52 +0100

    Update Othello.py

--- Othello/Othello.py
+++ Othello/Othello.py
@@ -24,8 +24,11 @@ class Config:
 
 # Calculates score if agent drops piece in selected column
 def score_move(grid, move, mark, nsteps):
-    next_grid, taken = play_piece(grid, move, mark)
-    score = alpha_beta(next_grid, nsteps - 1, mark, -np.Inf, np.Inf)
+    alpha = -np.Inf
+    beta = np.Inf  
+    next_grid = play_piece(grid, move, mark)[0]
+
+    score = alpha_beta_nega(next_grid, nsteps - 1, mark, False, alpha, beta)
     return score
 
 
@@ -44,7 +47,7 @@ def diag_bound(move):
             """top part"""
             boundary_ul = x - y
             boundary_dr = Config.size - x + y
-            boundaries = [[boundary_dr, -1],
+            boundaries = [[boundary_dr, Config.size],
                           [boundary_dl, 0],
                           [0, boundary_ur],
                           [0, boundary_ul]]
@@ -52,7 +55,7 @@ def diag_bound(move):
             """left part"""
             boundary_ul = y - x
             boundary_dr = Config.size - y + x
-            boundaries = [[-1, boundary_dr],
+            boundaries = [[Config.size, boundary_dr],
                           [boundary_dl, 0],
                           [0, boundary_ur],
                           [boundary_ul, 0]]
@@ -63,39 +66,48 @@ def diag_bound(move):
             """right part"""
             boundary_ul = x - y
             boundary_dr = Config.size - x + y
-            boundaries = [[boundary_dr, -1],
-                          [-1, boundary_dl],
-                          [boundary_ur, -1],
+            boundaries = [[boundary_dr, Config.size],
+                          [Config.size, boundary_dl],
+                          [boundary_ur, Config.size],
                           [0, boundary_ul]]
         else:
             """down part"""
             boundary_ul = y - x
             boundary_dr = Config.size - y + x
-            boundaries = [[-1, boundary_dr],
-                          [-1, boundary_dl],
-                          [boundary_ur, -1],
+            boundaries = [[Config.size, boundary_dr],
+                          [Config.size, boundary_dl],
+                          [boundary_ur, Config.size],
                           [boundary_ul, 0]]
     return boundaries
 
 
-def alpha_beta(node, depth, mark, alpha, beta):
-    is_terminal = is_terminal_node(node)
-    if depth == 0 or is_terminal:
+def alpha_beta_nega(node, depth, mark, maximizing, alpha, beta):
+
+    if depth == 0 or is_terminal_node(node):
         value = get_heuristic(node, mark)
-    else:
-        value = -np.Inf
-        for move in valid_moves(grid, mark):
-            child, child_taken = play_piece(node, move, mark)
-            value = max(value, alpha_beta(child, depth - 1, 1 - mark, - alpha,
-                        - beta))
-            alpha = max(alpha, value)
+        return value
+    elif maximizing:
+        alpha_node = -np.Inf
+        for move in valid_moves(node, mark):
+            child = play_piece(node, move, mark)[0]
+            value = alpha_beta_nega(child, depth - 1, mark, False, alpha, beta)
+            alpha_node = max(alpha_node, value)
+            alpha = max(alpha, alpha_node)
             if alpha >= beta:
                 break
-    print("alpha: ", file=sys.stderr, flush=True)
-    print(alpha, file=sys.stderr, flush=True)
-    print("beta: ", file=sys.stderr, flush=True)
-    print(beta, file=sys.stderr, flush=True)
-    return value
+        return alpha
+    else:
+        beta_node = np.Inf
+        for move in valid_moves(node, 1-mark):
+            child = play_piece(node, move, 1-mark)[0]
+            value = alpha_beta_nega(child, depth - 1, mark, True, alpha, beta)
+            beta_node = min(beta_node, value)
+            beta = min(beta, beta_node)
+
+            if alpha >= beta:
+                break
+
+        return beta
 
 
 # Calculates number of pieces in borders
@@ -111,49 +123,47 @@ def count_borders(grid, mark):
 
 def count_bef_borders(grid, mark):
     """Calculate number of pieces in the line before borders"""
-    up = grid[1, 2:-2]
-    down = grid[Config.size - 1, 2:-2]
-    left = grid[2:-2, 1]
-    right = grid[2:-2, Config.size - 1]
-    bef_borders = np.array(list(up) + list(down) + list(left) + list(right))
-    count = np.sum(bef_borders == str(mark))
-    """print("bef_borders: ", file=sys.stderr, flush=True)
-
-    print(bef_borders, file=sys.stderr, flush=True)
-    """
-
-    return count
+    win = np.array([])
+    if '.' in grid[0, 2:-2]:
+        win = np.append(win, grid[1, 2:-2])
+    if '.' in grid[-1, 2:-2]:
+        win = np.append(win, grid[-2, 2:-2])
+    if '.' in grid[2:-2, 0]:
+        win = np.append(win, grid[2:-2, 1])
+    if '.' in grid[2:-2, -1]:
+        win = np.append(win, grid[2:-2, -2])
+    return np.sum(win == str(mark))
 
 
 def count_bef_corners(grid, mark):
     """Calculate number of pieces before corners"""
-    win = list()
+    win = np.array([])
+    op = 1 - mark
     if grid[0, 0] == '.':
-        if str(1-mark) in grid[0, 2:] or '.' in grid[0, 2:]:
-            win.append(grid[0, 1])
-        if str(1-mark) in grid[2:, 0] or '.' in grid[2:, 0]:
-            win.append(grid[1, 0])
-        win.append(grid[1, 1])
+        if str(op) in grid[0, 2:] or '.' in grid[0, 2:]:
+            win = np.append(win, grid[0, 1])
+        if str(op) in grid[2:, 0] or '.' in grid[2:, 0]:
+            win = np.append(win, grid[1, 0])
+        win = np.append(win, grid[1, 1])
     if grid[0, -1] == '.':
-        if str(1-mark) in grid[0, :-2] or '.' in grid[0, :-2]:
-            win.append(grid[0, -2])
-        if str(1-mark) in grid[2:, -1] or '.' in grid[2:, -1]:
-            win.append(grid[1, -1])
-        win.append(grid[1, -2])
+        if str(op) in grid[0, :-2] or '.' in grid[0, :-2]:
+            win = np.append(win, grid[0, -2])
+        if str(op) in grid[2:, -1] or '.' in grid[2:, -1]:
+            win = np.append(win, grid[1, -1])
+        win = np.append(win, grid[1, -2])
     if grid[-1, 0] == '.':
-        if str(1-mark) in grid[-1, 2:] or '.' in grid[-1, 2:]:
-            win.append(grid[-1, 1])
-        if str(1-mark) in grid[:-2, 0] or '.' in grid[:-2, 0]:
-            win.append(grid[0, -2])
-        win.append(grid[-2, 1])
+        if str(op) in grid[-1, 2:] or '.' in grid[-1, 2:]:
+            win = np.append(win, grid[-1, 1])
+        if str(op) in grid[:-2, 0] or '.' in grid[:-2, 0]:
+            win = np.append(win, grid[0, -2])
+        win = np.append(win, grid[-2, 1])
     if grid[-1, -1] == '.':
-        if str(1-mark) in grid[-1, :-2] or '.' in grid[-1, :-2]:
-            win.append(grid[-1, -2])
-        if str(1-mark) in grid[:-2, -1] or '.' in grid[:-2, -1]:
-            win.append(grid[-2, -1])
-        win.append(grid[-2, -2])
-    count = np.sum(win == str(mark))
-    return count
+        if str(op) in grid[-1, :-2] or '.' in grid[-1, :-2]:
+            win = np.append(win, grid[-1, -2])
+        if str(op) in grid[:-2, -1] or '.' in grid[:-2, -1]:
+            win = np.append(win, grid[-2, -1])
+        win = np.append(win, grid[-2, -2])
+    return np.sum(win == str(mark))
 
 
 # Calculates number of pieces in corners
@@ -163,11 +173,10 @@ def count_corners(grid, mark):
     down_left = grid[Config.size, 0]
     down_right = grid[Config.size, Config.size]
     corners = np.array([up_left, down_left, up_right, down_right])
-    count = np.sum(corners == str(mark))
-    return count
+    return np.sum(corners == str(mark))
 
 
-# Helper function for listing vald moves
+# Helper function for listing valid moves
 def valid_moves(grid, mark):
     return [move for move in itertools.product(range(Config.size + 1),
                                                range(Config.size + 1))
@@ -195,60 +204,50 @@ def play_piece(grid, move, mark):
     y = move[0]
     next_grid = grid.copy()
     taken_this_move = 0
-    if grid[x, y] == '.':
-        next_grid[y, x] = mark
-        # horizontal +
-        for a in [1, -1]:
-            win_hor = grid[y, x::a]
-            win_ver = grid[y::a, x]
-            if len(win_hor) > 2 and win_hor[1] == str(1 - mark):
-                taken = taken_in_move(win_hor, mark)
-                if taken > 0:
-                    next_grid[y, x:x + a * taken] = mark
-                    taken_this_move += taken
-            if len(win_ver) > 2 and win_ver[1] == str(1 - mark):
-                taken = taken_in_move(win_ver, mark)
-                if taken > 0:
-                    next_grid[y:y + a * taken, x] = mark
-                    taken_this_move += taken
-        # diagonals
-        boundaries = diag_bound(move)
-        for (a, b), (bound_row, bound_col) in zip(itertools.product([1, -1], [1, -1]), boundaries):
-            diag = np.array([[row for row in range(y, bound_row + a, a)],
-                             [col for col in range(x, bound_col + b, b)]])
-            window = grid[tuple(diag)]
-            if len(window) > 2 and window[1] == str(1 - mark):
-                taken = taken_in_move(window, mark)
-                if taken > 0:
-                    next_grid[tuple(diag[:, :taken + 1])] = mark
-                    taken_this_move += taken
+    next_grid[y, x] = mark
+    # horizontal +
+    for a in [1, -1]:
+        win_hor = next_grid[y, x::a]
+        win_ver = next_grid[y::a, x]
+        if len(win_hor) > 2 and win_hor[1] == str(1 - mark):
+            taken = taken_in_move(win_hor, mark)
+            if taken > 0:
+                next_grid[y, x:x + a * (taken+1):a] = mark
+                taken_this_move += taken
+
+        if len(win_ver) > 2 and win_ver[1] == str(1 - mark):
+            taken = taken_in_move(win_ver, mark)
+            if taken > 0:
+                next_grid[y:y + a * (taken+1):a, x] = mark
+                taken_this_move += taken
+
+    # diagonals
+    boundaries = diag_bound(move)
+    for (a, b), (bound_row, bound_col) in zip(itertools.product([1, -1],
+                                              [1, -1]), boundaries):
+        diag = np.array([[row for row in range(y, bound_row + a, a)],
+                        [col for col in range(x, bound_col + b, b)]])
+        window = next_grid[tuple(diag)]
+        if len(window) > 2 and window[1] == str(1 - mark):
+            taken = taken_in_move(window, mark)
+            if taken > 0:
+                next_grid[tuple(diag[:, :taken + 1])] = mark
+                taken_this_move += taken
     return next_grid, taken_this_move
 
 
 # Helper function for score_move: calculates value of heuristic for grid
 def get_heuristic(grid, mark):
-    op = 1 - mark
+    list_grid = grid.ravel()
     corners = count_corners(grid, mark)
     borders = count_borders(grid, mark)
     bef_borders = count_bef_borders(grid, mark)
     bef_corners = count_bef_corners(grid, mark)
-    cant_play = not bool(valid_moves(grid, mark))
-    op_bef_borders = count_bef_borders(grid, op)
-    op_bef_corners = count_bef_corners(grid, op)
-    op_cant_play = not bool(valid_moves(grid, op))
-    op_corners = count_corners(grid, op)
-    op_borders = count_borders(grid, op)
-    score = ((grid == "mark").sum() - (grid == str(1 - mark)).sum()
-             + 10 * op_bef_borders
-             - 1000 * bef_borders
-             + 100000 * borders
-             - 5000 * op_borders
-             + 500000 * op_bef_corners
-             - 10000000 * bef_corners
-             + 1000000000 * corners
-             - 1000000000 * op_corners
-             + 100000000000 * op_cant_play
-             - 10000000000 * cant_play)
+    score = (np.sum(list_grid == str(mark))
+             - 100 * bef_borders
+             + 1000 * borders
+             - 100000 * bef_corners
+             + 10000000 * corners)
     return score
 
 
@@ -264,7 +263,13 @@ def agent(obs):
     v_moves = valid_moves(grid, obs.mark)
     print("v_moves: ", file=sys.stderr, flush=True)
     print(v_moves, file=sys.stderr, flush=True)
-    n_steps = 4
+
+    if len(v_moves) == 4:
+        n_steps = 3
+    elif len(v_moves) <= 3:
+        n_steps = 4
+    else:
+        n_steps = 2
     """ Use the heuristic to assign a score
      to each possible board in the next turn """
     scores = dict(zip(v_moves, [score_move(grid, move, obs.mark, n_steps)
@@ -279,7 +284,6 @@ def agent(obs):
     print("max_moves :", file=sys.stderr, flush=True)
 
     print(max_moves, file=sys.stderr, flush=True)
-
     return random.choice(max_moves)
 
 
@@ -313,5 +317,4 @@ while True:
 
 # if __name__ == '__main__':
 #    main()
-
 
